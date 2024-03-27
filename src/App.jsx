7c3b4c240ed091f6fe20ca34d22e0d2bb08ddfe6
Debasis Mishra <debasismishra000123@gmail.com>
2024-03-27 23:16:34 +0530

    Start

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './Components/Home'
+import NavBar from './Components/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import heroimg from "../assets/home-main.svg";
+import { IoIosArrowForward } from "react-icons/io";
+
+const Home = () => {
+  return (
+    <div
+      name="Home"
+      className=" h-screen w-full bg-gradient-to-b from-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center items-center h-full md:items-start">
+          <h2 className="text-4xl sm:text-4xl font-bold text-white">
+            Hey There 👋
+          </h2>
+          <h2 className="py-10 text-4xl sm:text-4xl font-bold text-white">
+            I'M <span className="text-Primary">DEBASIS MISHRA</span>
+          </h2>
+          <h2 className="py-10 text-4xl sm:text-4xl font-bold text-Primary">
+            Software Developer
+          </h2>
+          <div>
+            <button className="text-white w-fit px-6 py-3 my-2 flex  items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer group font-bold">
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <IoIosArrowForward size={25} />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            className="rounded-2xl mx-24 w-auto h-96 md:w-full"
+            src={heroimg}
+            alt="heroimg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
