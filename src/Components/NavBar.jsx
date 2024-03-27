4824b9e3b8a9fd4620de59b010d77bcdaf9a0283
Debasis Mishra <debasismishra000123@gmail.com>
2024-03-28 00:48:57 +0530

    Modified

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import { FaTimes } from "react-icons/fa";
 import { IoHomeOutline } from "react-icons/io5";
@@ -6,40 +6,36 @@ import { FaRegUser } from "react-icons/fa6";
 import { MdOutlineComputer } from "react-icons/md";
 import { FiPhone } from "react-icons/fi";
 
-
-
 const NavBar = () => {
+  const [nav, setnav] = useState(false);
 
-  const[nav,setnav]=useState(false);
-
-
-const linkitem=[
-  {
-    id:1,
-  Link:'Home',
-  icon:<IoHomeOutline />,
-  },
-  {
-    id:2,
-  Link:'about',
-  icon:<FaRegUser />,
-  },
-  {
-    id:3,
-  Link:'portfolio',
-  icon:<IoHomeOutline />,
-  },
-  {
-    id:4,
-  Link:'Experience',
-  icon:<MdOutlineComputer />,
-  },
-  {
-    id:5,
-  Link:'contact',
-  icon:<FiPhone />,
-  },
-]
+  const linkitem = [
+    {
+      id: 1,
+      Link: "Home",
+      icon: <IoHomeOutline />,
+    },
+    {
+      id: 2,
+      Link: "about",
+      icon: <FaRegUser />,
+    },
+    {
+      id: 3,
+      Link: "portfolio",
+      icon: <IoHomeOutline />,
+    },
+    {
+      id: 4,
+      Link: "Experience",
+      icon: <MdOutlineComputer />,
+    },
+    {
+      id: 5,
+      Link: "contact",
+      icon: <FiPhone />,
+    },
+  ];
 
   return (
     <div className="w-full h-16 px-4 bg-navbg items-center flex justify-between fixed">
@@ -48,25 +44,37 @@ const linkitem=[
       </div>
 
       <ul className="hidden md:flex">
-        {linkitem.map((link)=>(
-          <li key={link.id} className="px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-200">{link.Link}</li>
-        ))
-        }
+        {linkitem.map((link) => (
+          <li
+            key={link.id}
+            className="px-4 cursor-pointer capitalize font-medium text-white hover:scale-105 duration-200  hover:text-Primary"
+          >
+            {link.Link}
+          </li>
+        ))}
       </ul>
-      <div onClick={()=>setnav(!nav)} className="cursor-pointer pr-4 z-10 text-Primary md:hidden">
-      {nav ? <FaTimes size={30}/>:<FaBars size={30} />}
+      <div
+        onClick={() => setnav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-Primary md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
       </div>
 
-      {nav &&<ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-fit bg-gradient-to-b from-black to-gray-800 text-white md:hidden ">
-
-      {linkitem.map((link)=>(
-          <li key={link.id} className="px-4 cursor-pointer capitalize py-6 text-3xl hover:scale-105 duration-200 flex gap-x-2">{link.icon}{link.Link}</li>
-        ))
-        }
-      </ul>
-      }
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-fit bg-gradient-to-b from-black to-gray-800 text-white md:hidden ">
+          {linkitem.map((link) => (
+            <li
+              key={link.id}
+              className="px-4 cursor-pointer capitalize py-6 text-3xl hover:scale-105 duration-200 flex gap-x-2  hover:text-Primary"
+            >
+              {link.icon}
+              {link.Link}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
