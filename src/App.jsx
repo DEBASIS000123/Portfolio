8b92432ff0cde14acb0fad6533cc502a2fa32812
Debasis Mishra <debasismishra000123@gmail.com>
2024-03-28 00:47:30 +0530

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import './App.css'
-import Home from './Components/Home'
-import NavBar from './Components/NavBar'
+import "./App.css";
+import Home from "./Components/Home";
+import NavBar from "./Components/NavBar";
+import SocialLinks from "./Components/SocialLinks";
 
 function App() {
-
   return (
     <>
-      <NavBar/>
-      <Home/>
+      <NavBar />
+      <Home />
+
+      <SocialLinks />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
